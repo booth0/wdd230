b07e04fff0259ccab07d58bbcd3a3896b370d937
booth0 <122390745+booth0@users.noreply.github.com>
2024-05-04 20:22:51 -0600

    finish front page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,6 @@
 const year = document.querySelector('#year');
 const options = {year: "numeric"};
-// year.textContent += " " + new Date().toLocaleDateString("en-UK", options);
+
 const currentDate = new Date().toLocaleDateString("en-UK", options);
 
 // Split the existing content by comma
@@ -8,3 +8,13 @@ const contentArray = year.textContent.split(',');
 
 // Reconstruct the content with the date inserted between "&copy;" and "Adam Booth, Arizona"
 year.textContent = contentArray[0] + currentDate  + ' ' + contentArray[1] + ", " + contentArray[2];
+
+
+
+var lastModified = document.lastModified;
+            
+// Find the last modified paragraph element
+ var lastModifiedParagraph = document.getElementById("lastModified");
+            
+// Update the content of the paragraph with the last modified date and time
+lastModifiedParagraph.textContent += lastModified;

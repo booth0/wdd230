f7427cf8f8e4a1081a271a63d16521c05aa9dd95
booth0 <122390745+booth0@users.noreply.github.com>
2024-05-07 17:41:52 -0600

    design/bom

---
+++ week02/scripts/bom.js
@@ -0,0 +1,45 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value) {
+        // create an li element
+        const li = document.createElement('li');
+
+        // Create a delete button
+        const deleteButton = document.createElement('button');
+
+        //populate the li elements textContent or innerHTML with the input value
+        li.textContent = input.value;
+
+        // populate the button textContent with a ❌
+        deleteButton.textContent = '❌';
+        
+        // append the li element with the delete button
+        li.appendChild(deleteButton);
+
+        // append the li element to the unordered list in your HTML
+        list.appendChild(li);
+
+        //add an event listener to the delete button that removes the li element when clicked
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        })
+
+        // send the focus to the input element
+        input.focus();
+
+        // change the input value to nothing or the empty string to clean up the interface for the user
+        input.value = '';
+    }
+    else {
+        input.focus();
+    }
+
+    
+
+
+})
+

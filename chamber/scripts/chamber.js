2b67ab80fe053a06063c4fe8bf9da9d8054dc787
booth0 <122390745+booth0@users.noreply.github.com>
2025-02-20 16:34:31 -0700

    all

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,24 @@
+const year = document.querySelector('#year');
+const options = {year: "numeric"};
+
+const currentDate = new Date().toLocaleDateString("en-UK", options);
+
+// Split the existing content by comma
+const contentArray = year.textContent.split(',');
+
+// Reconstruct the content with the date inserted between "&copy;" and "Adam Booth, Arizona"
+year.textContent = contentArray[0] + currentDate  + ' ' + contentArray[1];
+
+
+
+var lastModified = document.lastModified;
+            
+// Find the last modified paragraph element
+ var lastModifiedParagraph = document.getElementById("lastModified");
+            
+// Update the content of the paragraph with the last modified date and time
+lastModifiedParagraph.textContent += lastModified;
+
 const hamButton = document.querySelector('#hamMenu');
 const navigation = document.querySelector('.navbar');
 

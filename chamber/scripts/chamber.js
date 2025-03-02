a2db0c1fb16c549e20c53ae32a7e15daf0a15905
booth0 <122390745+booth0@users.noreply.github.com>
2025-03-01 17:31:33 -0700

    dark mode and positioning

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -11,6 +11,8 @@ year.textContent = contentArray[0] + currentDate  + ' ' + contentArray[1];
 
 
 
+// Last Modified
+
 var lastModified = document.lastModified;
             
 // Find the last modified paragraph element
@@ -19,6 +21,10 @@ var lastModified = document.lastModified;
 // Update the content of the paragraph with the last modified date and time
 lastModifiedParagraph.textContent += lastModified;
 
+
+
+// Hamburger Menu
+
 const hamButton = document.querySelector('#hamMenu');
 const navigation = document.querySelector('.navbar');
 
@@ -26,3 +32,36 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+
+// Dark Mode
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const cards = document.querySelectorAll(".card");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		header.style.background = "#181818";
+		body.style.background = "#212121";
+		footer.style.background = "#181818";
+		modeButton.textContent = "🔆";
+		cards.forEach(card => {
+            card.style.background = "#181818";
+            card.style.color = "white";
+        });
+	} else {
+		header.style.background = "#17183B";
+		body.style.background = "#E9E6D3";
+		footer.style.background = "#A11692";
+		modeButton.textContent = "🕶️";
+		cards.forEach(card => {
+            card.style.background = "white";
+            card.style.color = "black";
+        });
+	}
+});
+
